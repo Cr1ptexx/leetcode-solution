708816493fec037a35b67146d68ba456e0a847e6
Cr1ptexx <vateekarn.w@gmail.com>
2026-07-15 15:32:31 +0700

    Time: 8 ms (13.06%) | Memory: 8.5 MB (87.36%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        unsigned int i = 1;
+        while(i*i < x){
+            i++;
+        }
+        if(i*i == x) return i;
+        else return i-1;
+    }
+};
