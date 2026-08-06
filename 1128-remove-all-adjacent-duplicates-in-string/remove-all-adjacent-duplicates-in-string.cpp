597e776e327534cc87da4d79ec7f5e659af5272e
Cr1ptexx <vateekarn.w@gmail.com>
2026-08-06 21:32:18 +0700

    Time: 723 ms (7.66%) | Memory: 783.5 MB (7.29%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+       stack<char> st;
+       for(char x : s){
+            if(st.empty()) st.push(x);
+            else if(st.top()==x){
+                st.pop();
+            }
+            else st.push(x);
+       }
+       string ans = "";
+       while(!st.empty()){
+            ans = st.top() + ans; st.pop();
+       }
+       return ans;
+    }
+};
